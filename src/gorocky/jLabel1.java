f5d8786936ae9eb238a6b564e40f3d1b2bba1944
dawadi <dawadi@dawadi-PC>
2017-10-28 12:10:04 -0700

    pictures for gui

---
+++ src/gorocky/jLabel1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gorocky;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author dawadi
+ */
+class jLabel1 {
+
+    static void setIcon(ImageIcon II) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
