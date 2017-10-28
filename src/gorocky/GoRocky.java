49199e25bc18729f6dacddc97bcd7d97de1d7b1b
dawadi <dawadi@dawadi-PC>
2017-10-28 11:56:50 -0700

    double instantiation removed

---
+++ src/gorocky/GoRocky.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gorocky;
+
+/**
+ *
+ * @author dawadi
+ */
+public class GoRocky {
+
+    /**
+     * @param args the command line arguments
+     */
+    //public FlappyBird flappybird;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //FlappyBird fb = new FlappyBird();
+        //fb.main(args);
+        Gui mygui= new Gui();    
+        mygui.show();
+        //mygui.setVisible(true); 
+        //String[] args = {};
+        //flappybird = new FlappyBird();
+        //flappybird.FlappyBird();
+
+    }
+   
+}
